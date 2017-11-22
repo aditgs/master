7da89095bd25a740b4aff9c0221710400fcd242b
Syahroni Wahyu <roniwahyu@gmail.com>
2017-11-22 19:41:36 +0700

    siakad dewa colo
    masukkan module siakad dewa colo

---
+++ dewantara112017/modules/keuangan/siakad_keu_paket_komp/views/siakad_keu_paket_komp_data.php
@@ -0,0 +1,27 @@
+<div class="datatable-ajax-source">
+        <div class="btn-group" style="margin:20px 0px 30px">
+                            <a href="<?php echo base_url('siakad_keu_paket_komp/baru') ?>" class="btn btn-lg btn-success"><i class="fa fa-plus"></i> Buka siakad_keu_paket_komp Baru </a>
+                            <a href="<?php echo base_url('siakad_keu_paket_komp') ?>" class="btn btn-lg btn-info"><i class="fa fa-database"></i> Data siakad_keu_paket_komp</a>
+                        </div>
+        <h2 class="text-center" style="margin:20px 0px 30px" >Tabel Data siakad_keu_paket_komp</h2>
+    
+                                <table id="datatables" class="table table-bordered table-condensed table-striped" style="">
+                                    <thead class="">
+                                        <tr>
+                                                       
+                                                        <th>id_siakad_keu_paket</th>
+                                                        <th>id_siakad_keu_master</th>
+                                                        <th>Aksi</th>
+
+                                                    </tr>
+                                    </thead>
+
+                                    <tbody class="table-bordered">
+                                        <tr>
+                                            <td colspan="3" class="text-center dataTables_empty"><img src="<?php echo assets_url('images/loader.gif');  ?>" title="Loading" alt="Loading">&nbsp;&nbsp; Loading data, please wait....</td>
+                                            
+                                        </tr>
+                                        
+                                    </tbody>
+                                </table>
+                            </div>
