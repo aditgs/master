b4f72561f0f7bb83400bc5ac2ade86933dddc0f7
Syahroni Wahyu <roniwahyu@gmail.com>
2017-12-06 18:20:03 +0700

    backup tagihan mhs
    backup tagihanmhs skema lama menjadi tagihanmhs2, skema baru tagihanmhs master detail seperti yang sudah dirancang

--- dewantara112017/config/uploads.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
-if($_SERVER['HTTP_HOST']=='localhost'):
-$config['uploads_url'] = "http://".$_SERVER['HTTP_HOST']."/uploads/";
-else:
-$config['uploads_url'] = "http://dev.jurnalstiedewantara.com/uploads/";
-endif;
-/* End of file uploads.php */
-/* Location: ./application/config/uploads.php */
