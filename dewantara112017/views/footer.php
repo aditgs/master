d3ff11f81e23752ab0635c9645449a005b0ec95e
Syahroni Wahyu <roniwahyu@gmail.com>
2018-01-06 07:12:54 +0700

    update commit PGRI
    update commit 44cfa8f79f25bbe476ad1033683d6464a86ee5a3

--- dewantara112017/views/footer.php
+++ dewantara112017/views/footer.php
@@ -1,7 +1,7 @@
 
 <div class="footer" style="bottom:0px;position:relative">
             <div>
-                Copyright <strong>STIE Dewantara</strong> &copy; 2018, <p >Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+                 Copyright <strong>STIE PGRI Dewantara</strong> &copy; 2018, <p >Page rendered in <strong>{elapsed_time}</strong> seconds</p> 
 
             </div>
         </div>
