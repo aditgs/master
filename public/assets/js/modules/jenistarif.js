1c17b85877c402033ff3cc6b87d0c9f154142708
Ahmad Rofiqul Muslikh <rofickachmad@icloud.com>
2018-01-25 14:03:20 +0700

    Update UI Modal dsb Jenis Tarif

---
+++ public/assets/js/modules/jenistarif.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $("#modal-form").on("hidden.bs.modal", function() {
+        // $('#modal-form .modal-body #addform #reset').trigger('click');
+        $('.modal-body').find('input').val('');
+    });
+    $("body").on("click", ".edit_jenis_tarif", function(e) {
+        e.preventDefault();
+        var id = $(this).attr("id");
+        $(this).ready(function() {
+            $.ajax({
+                // url: baseurl + "get/" + id,
+                url: baseurl + "get/",
+                data:{id:id},
+                type: "post",
+                dataType: "json",
+              	success: function(data) {
+              		
+                    for (var i in data) {
+                        // alert(data);
+                        $('body #modal-form .modal-body input[name="' + i + '"]').val(data[i]);
+                    }
+                    $('#body').val(data.body);
+                    $('body #modal-form .modal-body button#save').hide(200);
+                    $('body #modal-form .modal-body button#save_edit').fadeIn(200);
+
+                }
+            });
+
+        });
+
+    });
+    
+})
