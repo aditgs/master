a4070d5fc1d0a51239bd3cba2b4db44c46bf6aaf
Syahroni Wahyu <roniwahyu@gmail.com>
2018-02-01 18:07:16 +0700

    update tagihan select2 #2

--- public/assets/js/modules/loadselect2.js
+++ public/assets/js/modules/loadselect2.js
@@ -1,4 +1,4 @@
- $(".select2").select2({
+ $("#modal-form  .modal-body .select2").select2({
                 theme: "bootstrap input-md",
                 dropdownParent: "#modal-form"
             });
