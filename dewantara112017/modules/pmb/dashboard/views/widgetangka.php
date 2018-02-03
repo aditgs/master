d8dd5d684bf437f64b76d6f8e5ae2dac558f0d5a
Syahroni Wahyu <roniwahyu@gmail.com>
2018-02-03 20:33:24 +0700

    update dashboard pmb

--- dewantara112017/modules/pmb/dashboard/views/widgetangka.php
+++ dewantara112017/modules/pmb/dashboard/views/widgetangka.php
@@ -1,39 +1,39 @@
 <div class="col-lg-3">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <span class="label label-success pull-right">Monthly</span>
-                    <h5>Income</h5>
+                    <span class="label label-success pull-right">Gelombang 1</span>
+                    <h5>Total Pendaftar</h5>
                 </div>
                 <div class="ibox-content">
                     <h1 class="no-margins">40 886,200</h1>
                     <div class="stat-percent font-bold text-success">98% <i class="fa fa-bolt"></i></div>
-                    <small>Total income</small>
+                    <small>Total Calon Mahasiswa</small>
                 </div>
             </div>
         </div>
         <div class="col-lg-3">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <span class="label label-info pull-right">Annual</span>
-                    <h5>Orders</h5>
+                    <span class="label label-info pull-right">Gelombang 1</span>
+                    <h5>Prodi Akuntansi</h5>
                 </div>
                 <div class="ibox-content">
                     <h1 class="no-margins">275,800</h1>
                     <div class="stat-percent font-bold text-info">20% <i class="fa fa-level-up"></i></div>
-                    <small>New orders</small>
+                     <small>Total Calon Mahasiswa</small>
                 </div>
             </div>
         </div>
         <div class="col-lg-3">
             <div class="ibox float-e-margins">
                 <div class="ibox-title">
-                    <span class="label label-primary pull-right">Today</span>
-                    <h5>Vistits</h5>
+                    <span class="label label-primary pull-right">Gelombang 1</span>
+                    <h5>Prodi Manajemen</h5>
                 </div>
                 <div class="ibox-content">
                     <h1 class="no-margins">106,120</h1>
                     <div class="stat-percent font-bold text-navy">44% <i class="fa fa-level-up"></i></div>
-                    <small>New visits</small>
+                     <small>Total Calon Mahasiswa</small>
                 </div>
             </div>
         </div>
