22ca633d04c2a31bb988c8c2de9d598124f9b180
Syahroni Wahyu <roniwahyu@gmail.com>
2018-02-03 16:47:54 +0700

    update datepicker pmb

--- dewantara112017/modules/pmb/mhspmb/controllers/mhspmb.php
+++ dewantara112017/modules/pmb/mhspmb/controllers/mhspmb.php
@@ -20,15 +20,14 @@ class mhspmb extends MX_Controller {
         $this->template->set_layout('dashboard');
 
         /*UNTUK KEPERLUAN FORM*/
-        /*$this->template->add_js('accounting.min.js');
+ /*       $this->template->add_js('accounting.min.js');
         $this->template->add_js('jquery.maskMoney.min.js');   
         $this->template->add_css('plugins/datapicker/datepicker3.css');
         $this->template->add_js('plugins/datapicker/bootstrap-datepicker.js');
-        $this->template->add_js('datepicker.js'); //tanggal
         $this->template->add_js('plugins/select2/select2.min.js');
         $this->template->add_css('plugins/select2/select2.min.css');
-        $this->template->add_css('plugins/select2/select2-bootstrap.min.css');*/
-        
+        $this->template->add_css('plugins/select2/select2-bootstrap.min.css');
+        */
         /*ONLINE CDN*/
         $this->template->add_css('https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker.min.css','cdn');
         $this->template->add_js('https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.min.js','cdn');
@@ -38,6 +37,7 @@ class mhspmb extends MX_Controller {
         $this->template->add_js('https://cdn.jsdelivr.net/jquery.maskmoney/3.0.2/jquery.maskMoney.min.js','cdn');
         $this->template->add_js('https://cdn.jsdelivr.net/accounting.js/0.3.2/accounting.min.js','cdn');
         
+        $this->template->add_js('datepicker.js'); //tanggal
     }
 
     public function index() {
