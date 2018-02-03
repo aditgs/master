16b52087ca73ab7c9895170b0ee4c5a1f6b93687
Ahmad Rofiqul Muslikh <rofickachmad@icloud.com>
2018-02-03 17:03:29 +0700

    UpdatePMBHS

--- dewantara112017/modules/pmb/mhspmb/models/siakad_mhs_pmb_model.php
+++ dewantara112017/modules/pmb/mhspmb/models/siakad_mhs_pmb_model.php
@@ -23,7 +23,7 @@ class Siakad_mhs_pmb_model extends CI_Model {
         $this->db->where('datetime',NULL);
         $this->db->where('tanggal',NULL);
         $this->db->where('islocked',NULL);
-        $this->db->order_by('id_siakad_mhs_pmb','ASC');
+        $this->db->order_by('id','ASC');
         $this->db->limit(1);
 
         $result=$this->db->get('siakad_mhs_pmb');
@@ -36,7 +36,9 @@ class Siakad_mhs_pmb_model extends CI_Model {
     //untuk generate faktur baru
     function get_last(){
 
-        $this->db->select('*'); //faktur
+        $this->db->select('noreg_pmb'); //faktur
+        $x=substr($prodi,0,2);
+        $this->db->like('noreg_pmb',$x); //faktur
         $this->db->order_by('id_siakad_mhs_pmb','DESC');
         $this->db->limit(1);
 
@@ -84,6 +86,49 @@ class Siakad_mhs_pmb_model extends CI_Model {
             return array();
         }
     }
+    function genkode($prodi){
+        $last=$this->get_last($prodi);
+        // print_r($last);
+            // if(!empty($tahun)){
+            //     $numth=strlen($tahun);
+
+            //     if($numth=4){
+            //         $tahun=substr($tahun,-2);
+            //     }elseif($numth=2){
+            //         $tahun=$tahun;
+            //     }else{
+            //         $tahun='00';
+            //     }
+            // }else{
+            $tahun=date("y");
+            //     $th=$date;
+            // }
+            if(!empty($prodi)){
+                $num=strlen($prodi);
+                // echo $num;
+                if($num=5){
+                    $pro=substr($prodi,0,2);
+                }elseif($num=2){
+                    $pro;
+                }else{
+                    $pro='00';
+                }
+            }else{
+                $pro='00';
+            }
+        if(!empty($last)):
+            $x=$last['noreg_pmb'];
+            $y=substr($x,-4);
+            $z=strval($y)+1;
+            $a="0000".$z;
+            $b=substr($a,-4);
+            $gen=$pro.$tahun.$b;
+
+        else:
+           $gen=$pro.$tahun."0001";
+        endif;
+        return $gen;
+    }
     function genfaktur(){
         $last=$this->get_last();
         // print_r($last);
@@ -94,8 +139,8 @@ class Siakad_mhs_pmb_model extends CI_Model {
         endif;
         return ($faktur);
     }
-    function get_one($id_siakad_mhs_pmb) {
-        $this->db->where('id_siakad_mhs_pmb', $id_siakad_mhs_pmb);
+    function get_one($id) {
+        $this->db->where('id', $id);
         $result = $this->db->get('siakad_mhs_pmb');
         if ($result->num_rows() == 1) {
             return $result->row_array();
@@ -123,67 +168,42 @@ class Siakad_mhs_pmb_model extends CI_Model {
        $data = array(
         
             'kode_prodi' => $this->input->post('kode_prodi', TRUE),
-           
-            'id_siakad_kelas' => $this->input->post('id_siakad_kelas', TRUE),
-           
-            'tgl_reg_pmb' => $this->input->post('tgl_reg_pmb', TRUE),
-           
-            'noreg_pmb' => $this->input->post('noreg_pmb', TRUE),
-           
-            'nm_cmhs' => $this->input->post('nm_cmhs', TRUE),
-           
-            'kelamin_cmhs' => $this->input->post('kelamin_cmhs', TRUE),
-           
-            'tmp_cmhs' => $this->input->post('tmp_cmhs', TRUE),
-           
-            'tgl_cmhs' => $this->input->post('tgl_cmhs', TRUE),
-           
-            'agama_cmhs' => $this->input->post('agama_cmhs', TRUE),
-           
-            'almt_cmhs' => $this->input->post('almt_cmhs', TRUE),
-           
-            'kota_cmhs' => $this->input->post('kota_cmhs', TRUE),
-           
-            'kodepos_cmhs' => $this->input->post('kodepos_cmhs', TRUE),
-           
-            'email_cmhs' => $this->input->post('email_cmhs', TRUE),
-           
-            'hp_cmhs' => $this->input->post('hp_cmhs', TRUE),
-           
-            'telp_cmhs' => $this->input->post('telp_cmhs', TRUE),
-           
-            'asal_pend' => $this->input->post('asal_pend', TRUE),
-           
-            'jurusan_pend' => $this->input->post('jurusan_pend', TRUE),
-           
-            'no_ijazah_pend' => $this->input->post('no_ijazah_pend', TRUE),
-           
-            'tgl_ijazah_pend' => $this->input->post('tgl_ijazah_pend', TRUE),
-           
-            'nil_ijazah_pend' => $this->input->post('nil_ijazah_pend', TRUE),
-           
-            'status_pmb' => $this->input->post('status_pmb', TRUE),
-           
-            'id_siakad_keu_rek' => $this->input->post('id_siakad_keu_rek', TRUE),
-           
-            'id_siakad_keu_pendaftaran' => $this->input->post('id_siakad_keu_pendaftaran', TRUE),
-           
-            'tgl_transfer' => $this->input->post('tgl_transfer', TRUE),
-           
-            'nm_transfer' => $this->input->post('nm_transfer', TRUE),
-           
-            'img_bukti_transfer' => $this->input->post('img_bukti_transfer', TRUE),
-           
-            'img_pasfoto' => $this->input->post('img_pasfoto', TRUE),
-           
-            'img_ijazah' => $this->input->post('img_ijazah', TRUE),
-           
-            'img_transkrip' => $this->input->post('img_transkrip', TRUE),
-           
-            'img_pindah' => $this->input->post('img_pindah', TRUE),
-           
-            'status_cmhs' => $this->input->post('status_cmhs', TRUE),
-           
+             'id_siakad_kelas' => $this->input->post('id_siakad_kelas', TRUE),
+             'tgl_reg_pmb' =>NOW(),
+             'noreg_pmb' => $this->genkode($this->input->post('kode_prodi', TRUE)),
+             'nik_cmhs' => $this->input->post('nik_cmhs', TRUE),
+             'nm_cmhs' => $this->input->post('nm_cmhs', TRUE),
+             'kelamin_cmhs' => $this->input->post('kelamin_cmhs', TRUE),
+             'tmp_cmhs' => $this->input->post('tmp_cmhs', TRUE),
+             'tgl_cmhs' => $this->input->post('tgl_cmhs', TRUE),
+             'agama_cmhs' => $this->input->post('agama_cmhs', TRUE),
+             'almt_cmhs' => $this->input->post('almt_cmhs', TRUE),
+             'kota_cmhs' => $this->input->post('kota_cmhs', TRUE),
+             'kodepos_cmhs' => $this->input->post('kodepos_cmhs', TRUE),
+             'email_cmhs' => $this->input->post('email_cmhs', TRUE),
+             'hp_cmhs' => $this->input->post('hp_cmhs', TRUE),
+             'telp_cmhs' => $this->input->post('telp_cmhs', TRUE),
+             'asal_pend' => $this->input->post('asal_pend', TRUE),
+             'jurusan_pend' => $this->input->post('jurusan_pend', TRUE),
+             'no_ijazah_pend' => $this->input->post('no_ijazah_pend', TRUE),
+             'tgl_ijazah_pend' => $this->input->post('tgl_ijazah_pend', TRUE),
+             'nil_ijazah_pend' => $this->input->post('nil_ijazah_pend', TRUE),
+             'nm_ibu_cmhs' => $this->input->post('nm_ibu_cmhs', TRUE),
+             'status_pmb' => 'Baru',
+             'id_siakad_keu_rek' => $this->input->post('id_siakad_keu_rek', TRUE),
+             'id_siakad_keu_pendaftaran' => $this->input->post('id_siakad_keu_pendaftaran', TRUE),
+             'tgl_transfer' => $this->input->post('tgl_transfer', TRUE),
+             'nm_transfer' => $this->input->post('nm_transfer', TRUE),
+             'img_bukti_transfer' => $this->input->post('img_bukti_transfer', TRUE),
+             'img_pasfoto' => $this->input->post('img_pasfoto', TRUE),
+             'img_ijazah' => $this->input->post('img_ijazah', TRUE),
+             'img_transkrip' => $this->input->post('img_transkrip', TRUE),
+             'img_pindah' => $this->input->post('img_pindah', TRUE),
+             'status_cmhs' => $this->input->post('status_cmhs', TRUE),
+             'memo' => $this->input->post('memo', TRUE),
+             'gelid' => $this->input->post('gelid', TRUE),
+             'userid' =>userid(),
+
         );
         //'isdeleted' => null,
         //    'datedeleted' => null,
@@ -202,7 +222,7 @@ class Siakad_mhs_pmb_model extends CI_Model {
            
             'tgl_reg_pmb' => NOW(),
            
-            'noreg_pmb' => $this->input->post('noreg_pmb', TRUE),
+            'noreg_pmb' => $this->genkode($this->input->post('kode_prodi', TRUE)),
            
             'nm_cmhs' => $this->input->post('nm_cmhs', TRUE),
            
@@ -259,13 +279,7 @@ class Siakad_mhs_pmb_model extends CI_Model {
             'status_cmhs' => $this->input->post('status_cmhs', TRUE),
            
         );
-        //'isdeleted' => null,
-        //    'datedeleted' => null,
-        //    'islocked' =>1,
-        //    'isactive' => 1,
-        //    'userid' => userid(),
-        //    'datetime' => NOW(),
-        //    'Time' => NOW(),
+      
         return $data;
     }
     function savesiakad_mhs_pmb($data){
@@ -274,7 +288,7 @@ class Siakad_mhs_pmb_model extends CI_Model {
     function save_detail($data){
         $this->db->insert('siakad_mhs_pmb_detail',$data);
     }
-    function upddel_detail($id_siakad_mhs_pmb=null) {
+    function upddel_detail($id=null) {
         //semua field ini menyesuaikan dengan yang ada pada model dan tabel
         $data=array(
              'isdeleted' => 1,
@@ -286,16 +300,16 @@ class Siakad_mhs_pmb_model extends CI_Model {
 
             );
         
-        $this->db->where('id_siakad_mhs_pmb', $id_siakad_mhs_pmb);
+        $this->db->where('id', $id);
         $this->db->update('siakad_mhs_pmb', $data);
        
     } 
-    function updatebyid($id_siakad_mhs_pmb,$data){
-        $this->db->where('id_siakad_mhs_pmb', $id_siakad_mhs_pmb);
+    function updatebyid($id,$data){
+        $this->db->where('id', $id);
         $this->db->update('siakad_mhs_pmb', $data);
     }
-    function updcetak($id_siakad_mhs_pmb){
-        $cetak=$this->get_one($id_siakad_mhs_pmb);
+    function updcetak($id){
+        $cetak=$this->get_one($id);
         if(!empty($cetak)){
             if(isset($cetak['printcount'])||$cetak['printcount']>0){
                 $numcetak=$cetak['printcount']+1;
@@ -311,12 +325,12 @@ class Siakad_mhs_pmb_model extends CI_Model {
             'printcount'=>$numcetak,
             'userprinted'=>userid(),
         );
-        $this->db->where('id_siakad_mhs_pmb', $id_siakad_mhs_pmb);
+        $this->db->where('id', $id);
 
         $this->db->update('mhspmb', $data);
     }
     function gettagihan($id){
-        $this->db->select('*')->from('siakad_mhs_pmb')->where('id_siakad_mhs_pmb',$id);
+        $this->db->select('*')->from('001-view-tagihanmhs')->where('id',$id);
         $result=$this->db->get();
         if($result->num_rows()==1){
             return $result->row_array();
@@ -326,77 +340,52 @@ class Siakad_mhs_pmb_model extends CI_Model {
     }
     function update($id_siakad_mhs_pmb) {
         $data = array(
-        'id_siakad_mhs_pmb' => $this->input->post('id_siakad_mhs_pmb',TRUE),
+   
        'kode_prodi' => $this->input->post('kode_prodi', TRUE),
-       
        'id_siakad_kelas' => $this->input->post('id_siakad_kelas', TRUE),
-       
-       'tgl_reg_pmb' => NOW(),
-       
+       'tgl_reg_pmb' => $this->input->post('tgl_reg_pmb', TRUE),
        'noreg_pmb' => $this->input->post('noreg_pmb', TRUE),
-       
+       'nik_cmhs' => $this->input->post('nik_cmhs', TRUE),
        'nm_cmhs' => $this->input->post('nm_cmhs', TRUE),
-       
        'kelamin_cmhs' => $this->input->post('kelamin_cmhs', TRUE),
-       
        'tmp_cmhs' => $this->input->post('tmp_cmhs', TRUE),
-       
        'tgl_cmhs' => $this->input->post('tgl_cmhs', TRUE),
-       
        'agama_cmhs' => $this->input->post('agama_cmhs', TRUE),
-       
        'almt_cmhs' => $this->input->post('almt_cmhs', TRUE),
-       
        'kota_cmhs' => $this->input->post('kota_cmhs', TRUE),
-       
        'kodepos_cmhs' => $this->input->post('kodepos_cmhs', TRUE),
-       
        'email_cmhs' => $this->input->post('email_cmhs', TRUE),
-       
        'hp_cmhs' => $this->input->post('hp_cmhs', TRUE),
-       
        'telp_cmhs' => $this->input->post('telp_cmhs', TRUE),
-       
        'asal_pend' => $this->input->post('asal_pend', TRUE),
-       
        'jurusan_pend' => $this->input->post('jurusan_pend', TRUE),
-       
        'no_ijazah_pend' => $this->input->post('no_ijazah_pend', TRUE),
-       
        'tgl_ijazah_pend' => $this->input->post('tgl_ijazah_pend', TRUE),
-       
        'nil_ijazah_pend' => $this->input->post('nil_ijazah_pend', TRUE),
-       
+       'nm_ibu_cmhs' => $this->input->post('nm_ibu_cmhs', TRUE),
        'status_pmb' => $this->input->post('status_pmb', TRUE),
-       
        'id_siakad_keu_rek' => $this->input->post('id_siakad_keu_rek', TRUE),
-       
        'id_siakad_keu_pendaftaran' => $this->input->post('id_siakad_keu_pendaftaran', TRUE),
-       
        'tgl_transfer' => $this->input->post('tgl_transfer', TRUE),
-       
        'nm_transfer' => $this->input->post('nm_transfer', TRUE),
-       
        'img_bukti_transfer' => $this->input->post('img_bukti_transfer', TRUE),
-       
        'img_pasfoto' => $this->input->post('img_pasfoto', TRUE),
-       
        'img_ijazah' => $this->input->post('img_ijazah', TRUE),
-       
        'img_transkrip' => $this->input->post('img_transkrip', TRUE),
-       
        'img_pindah' => $this->input->post('img_pindah', TRUE),
-       
        'status_cmhs' => $this->input->post('status_cmhs', TRUE),
-       
+       'memo' => $this->input->post('memo', TRUE),
+       'gelid' => $this->input->post('gelid', TRUE),
+       'userid' =>userid(),
+
         );
-        $this->db->where('id_siakad_mhs_pmb', $id_siakad_mhs_pmb);
+        $this->db->where('id', $id);
         $this->db->update('siakad_mhs_pmb', $data);
         /*'datetime' => date('Y-m-d H:i:s'),*/
     }
 
-    function delete($id_siakad_mhs_pmb) {
-        $this->db->where('id_siakad_mhs_pmb', $id_siakad_mhs_pmb);
+    function delete($id) {
+        $this->db->where('id', $id);
         $this->db->delete('siakad_mhs_pmb'); 
        
     }
@@ -414,16 +403,36 @@ class Siakad_mhs_pmb_model extends CI_Model {
     //untuk Array Dropdown
     function get_dropdown_array($value){
         $result = array();
-        $array_keys_values = $this->db->query('select id_siakad_mhs_pmb, '.$value.' from siakad_mhs_pmb order by id_siakad_mhs_pmb asc');
-        $result[0]="-- Pilih Urutan id_siakad_mhs_pmb --";
+        $array_keys_values = $this->db->query('select id, '.$value.' from siakad_mhs_pmb order by id asc');
+        $result[0]="-- Pilih Urutan id --";
         foreach ($array_keys_values->result() as $row)
         {
-            $result[$row->id_siakad_mhs_pmb]= $row->$value;
+            $result[$row->id]= $row->$value;
+        }
+        return $result;
+    } 
+    function getdropkelas(){
+        $result = array();
+        $array_keys_values = $this->db->query('select id_siakad_kelas,inisial_kelas,nm_kelas from siakad_kelas where thn_akademik="2018" order by id_siakad_kelas asc ');
+        $result[0]="-- Pilih Kelas --";
+        foreach ($array_keys_values->result() as $row)
+        {
+            $result[$row->id_siakad_kelas]= $row->inisial_kelas." - ".$row->nm_kelas;
+        }
+        return $result;
+    }
+    function getdropgel(){
+        $result = array();
+        $array_keys_values = $this->db->query('select id,kodegel,keterangan,date_start,date_end from `001-view-gelpmbaktif` where isactive=1 order by id asc ');
+        $result[0]="-- Pilih Gelombang --";
+        foreach ($array_keys_values->result() as $row)
+        {
+            $result[$row->id]= $row->keterangan." <label class='label label-success'>(".$row->date_start." s/d ".$row->date_end.")</label>";
         }
         return $result;
     }
 
-    //Update 30122014 SWI
+    //Update 30222014 SWI
     //untuk Array Dropdown dari database yang lain
     function get_drop_array($db,$key,$value){
         $result = array();
