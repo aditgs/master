e69aa2c3e4c1428c3329e14f66435472328d610d
Syahroni Wahyu <roniwahyu@gmail.com>
2018-02-05 21:23:20 +0700

    oke

--- dewantara112017/modules/pmb/mhspmb/views/template-cetak-kartu.php
+++ dewantara112017/modules/pmb/mhspmb/views/template-cetak-kartu.php
@@ -6,20 +6,24 @@
     <title>A5</title>
     <link href="<?php echo assets_url() ?>css/bootstrap.min.css" rel="stylesheet">
     <!-- Normalize or reset CSS with your favorite library -->
-    <!-- <link href="<?php echo assets_url() ?>css/bootstrap.min.css" rel="stylesheet"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
     <!-- Load paper.css for happy printing -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paper-css/0.3.0/paper.css">
+    <!-- <link href="<?php echo assets_url() ?>css/style.css" rel="stylesheet"> -->
+    <!-- <link href="<?php echo assets_url() ?>css/custom.css" rel="stylesheet"> -->
     <!-- Set page size here: A5, A4 or A3 -->
     <!-- Set also "landscape" if you need -->
     <style>
     @page {
         size: A6
+
     }
 
     body.A6 .sheet {
         width: 105mm;
         height: 148mm
+        border-height:4px;
+        border-color: #000000;
     }
 
     body.A6.landscape .sheet {
@@ -29,6 +33,8 @@
 
     body.A6 {
         width: 105mm
+         border-height:4px;
+        border-color: #000000;
     }
         /** Padding area **/
     .sheet.padding-5mm { padding: 5mm }
@@ -65,120 +71,111 @@
     <section class="sheet padding-5mm">
         <!-- Write HTML just like a web page -->
         <article>
-            <?php if(isset($data)){ $detail=$this->pmbdb->getpmb($data['gelid']); print_r($detail)?>
+            <?php if(isset($data)){ $detail=$this->pmbdb->getpmb($data['gelid']); //print_r($detail)?>
             <!-- <table class="table table-striped table-hover table-bordered"> -->
 
             <table>
                 <thead>  <tr>
                     <th><img alt="image" style="width:75px;" src="<?= assets_url('images/logo.png') ?>" /></th>
-                    <th colspan="3">KARTU PESERTA
-                        <br> SELEKSI PMB GELOMBANG I
-                        <br> STIE PGRI DEWANTARA JOMBANG TA. 2018/2019
-                        <br>
+                    <th colspan="3"><h2>KARTU PESERTA</h2>
+                        <h3>SELEKSI PMB GELOMBANG I</h3>
+                        <h4>STIE PGRI DEWANTARA JOMBANG TA. 2018/2019</h4>
+                        
                     </th>
                 </tr>
             </thead>
 
             </table>
-            <div class="row">
-                
-                <div class="col-sm-4">
-                    <strong>No. Pendaftaran</strong>
+            <div class="row gutter5">
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                    <div style="display:block;float:left;border:1px solid #333;min-width: 30mm;min-height: 4mm; width: 150px;height:200px;"></div>
                 </div>
-                <div class="col-sm-8">
-                    <?php echo $data['noreg_pmb'] ?>
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                    <div class="row">
+                
+                        <div class="col-sm-4">
+                            <strong>No.</strong>
+                        </div>
+                        <div class="col-sm-8">
+                            <?php echo $data['noreg_pmb'] ?>
+                        </div>
+                    </div>  
+                    <div class="row">
+                        
+                        <div class="col-sm-4">
+                            <strong>Nama:</strong>
+                        </div>
+                        <div class="col-sm-8">
+                            <?php echo $data['nm_cmhs'] ?>
+                        </div>
+                    </div> 
+                    <div class="row">
+                        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                             <strong>Gelombang </strong>
+                        </div>
+                        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                            <?php echo $detail['keterangan']; ?>
+                        </div>
+
+                    </div> 
+                    <div class="row">
+                        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                            <strong>
+                            Tanggal Ujian:
+                            </strong>
+                        </div>
+                        <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+                                
+                            <?php echo date("d M Y", strtotime($detail['date_seleksi_start'])); ?>
+                        </div>
+
+                    </div> 
                 </div>
             </div>
-            <div class="row">
-                
-                <div class="col-sm-4">
-                    <strong>Nama Calon Mahasiswa</strong>
-                </div>
-                <div class="col-sm-8">
-                    <?php echo $data['nm_cmhs'] ?>
-                </div>
-            </div>
+            
+          
+           
+            
             <div class="row">
                 <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                    Gelombang
+                    <strong>
+                    Pengumuman:
+                    </strong>
                 </div>
                 <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-                    <?php echo $detail['keterangan']; ?>
-                </div>
-
-            </div> 
-            <div class="row">
-                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                    Tanggal Ujian:
-                </div>
-                <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
-                    <?php echo thedate($detail['date_seleksi_start']); ?>
+                        
+                    <?php echo date("d M Y", strtotime($detail['date_pengumuman'])); ?>
                 </div>
 
             </div>
-            <table border="0px" width="">
-              
-         
-                <tr>
-                    <td></td>
-                    <td colspan="2"></td>
-                    
-                    <td>
-                        <?php echo $data['nm_cmhs'] ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td colspan="2">Terbilang</td>
-                    
-                    <td>
-                        <?php echo terbilang($data['noreg_pmb']) ?>
-                    </td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td colspan="2">Untuk Pembayaran</td>
-                    
-                    <td>PENDAFTARAN MAHASISWA BARU</td>
-                </tr>
-                <tr>
-                    <td colspan="5">
-                        <br>
-                        <br> </td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td colspan="3"></td>
-                    <td>Jombang,
-                        <?php echo thedate($data['tgl_reg_pmb']) ?>
-                        <br> Penerima,
-                    </td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td colspan="3">Rp.
-                        <?php echo $data['noreg_pmb'] ?>
-                        <br>
-                        <br>
-                    </td>
-                    <td></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td colspan="3"></td>
-                    <td><?php 
-                      $user = $this->ion_auth->user()->row(); 
-            if (!empty($user)):
-                $userid=$user->id;
-                $username=$user->username; //untuk field $User
-                $first=$user->firstname; //untuk field $User
-                echo $first;
-            endif;
+             <div class="row">
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                  <?php echo "&nbsp;" ?> 
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                   <?php if ($this->ion_auth->logged_in()): 
+                   $user = $this->ion_auth->user()->row();
+                   echo "<p>Jombang, ".date("d M Y")."</p>"; ?>
+                    <p>Panitia PMB,</p>
+                    <p>
+                        
+                        <?php 
+                         if (!empty($user)):
+                            $userid=$user->id;
+                            $username=$user->username; //untuk field $User
+                            $first=$user->first_name; //untuk field $User
+                            echo $first;
+                        else:
+                            echo "halo";
+                        endif;endif; ?>
+                    </p>
+                   
 
-                     ?></td>
-                </tr>
-            </table>
+                </div>
+
+            </div>
+
+            
             <?php
 
 }else{
@@ -188,6 +185,21 @@
         </article>
     </section>
      <style type="text/css">
+/*     .row {
+    margin-right: -15px;
+    margin-left: -15px
+}*/
+.row{
+    line-height: 2;
+}
+.row:after,
+.row:before {
+    display: table;
+    content: " "
+}
+.row:after {
+    clear: both
+}
     /*https://gist.github.com/donnierayjones/6fd9802d992b2d8d6cfd*/
 /*@media print{*/
     .col-sm-1,.col-sm-10,.col-sm-11,.col-sm-12,.col-sm-2,.col-sm-3,.col-sm-4,.col-sm-5,.col-sm-6,.col-sm-7,.col-sm-8,.col-sm-9{float:left}.col-sm-12{width:100%}.col-sm-11{width:91.66666667%}.col-sm-10{width:83.33333333%}.col-sm-9{width:75%}.col-sm-8{width:66.66666667%}.col-sm-7{width:58.33333333%}.col-sm-6{width:50%}.col-sm-5{width:41.66666667%}.col-sm-4{width:33.33333333%}.col-sm-3{width:25%}.col-sm-2{width:16.66666667%}.col-sm-1{width:8.33333333%}.col-sm-pull-12{right:100%}.col-sm-pull-11{right:91.66666667%}.col-sm-pull-10{right:83.33333333%}.col-sm-pull-9{right:75%}.col-sm-pull-8{right:66.66666667%}.col-sm-pull-7{right:58.33333333%}.col-sm-pull-6{right:50%}.col-sm-pull-5{right:41.66666667%}.col-sm-pull-4{right:33.33333333%}.col-sm-pull-3{right:25%}.col-sm-pull-2{right:16.66666667%}.col-sm-pull-1{right:8.33333333%}.col-sm-pull-0{right:auto}.col-sm-push-12{left:100%}.col-sm-push-11{left:91.66666667%}.col-sm-push-10{left:83.33333333%}.col-sm-push-9{left:75%}.col-sm-push-8{left:66.66666667%}.col-sm-push-7{left:58.33333333%}.col-sm-push-6{left:50%}.col-sm-push-5{left:41.66666667%}.col-sm-push-4{left:33.33333333%}.col-sm-push-3{left:25%}.col-sm-push-2{left:16.66666667%}.col-sm-push-1{left:8.33333333%}.col-sm-push-0{left:auto}.col-sm-offset-12{margin-left:100%}.col-sm-offset-11{margin-left:91.66666667%}.col-sm-offset-10{margin-left:83.33333333%}.col-sm-offset-9{margin-left:75%}.col-sm-offset-8{margin-left:66.66666667%}.col-sm-offset-7{margin-left:58.33333333%}.col-sm-offset-6{margin-left:50%}.col-sm-offset-5{margin-left:41.66666667%}.col-sm-offset-4{margin-left:33.33333333%}.col-sm-offset-3{margin-left:25%}.col-sm-offset-2{margin-left:16.66666667%}.col-sm-offset-1{margin-left:8.33333333%}.col-sm-offset-0{margin-left:0}.visible-xs{display:none!important}.hidden-xs{display:block!important}table.hidden-xs{display:table}tr.hidden-xs{display:table-row!important}td.hidden-xs,th.hidden-xs{display:table-cell!important}.hidden-sm,.hidden-xs.hidden-print{display:none!important}.visible-sm{display:block!important}table.visible-sm{display:table}tr.visible-sm{display:table-row!important}td.visible-sm,th.visible-sm{display:table-cell!important}
