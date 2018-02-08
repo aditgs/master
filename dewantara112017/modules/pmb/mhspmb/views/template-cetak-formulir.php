91153bc6a6cd821a80e9853d03686f3c8ed8fb1f
Syahroni Wahyu <roniwahyu@gmail.com>
2018-02-09 06:46:43 +0700

    fix blank page pas print

--- dewantara112017/modules/pmb/mhspmb/views/template-cetak-formulir.php
+++ dewantara112017/modules/pmb/mhspmb/views/template-cetak-formulir.php
@@ -79,7 +79,7 @@
 <body class="A4">
     <!-- Each sheet element should have the class "sheet" -->
     <!-- "padding-**mm" is optional: you can set 10, 15, 20 or 25 -->
-    <section>
+    <section class="no-print">
         <div class="text-center">
                 <div class="btn-group" style="">
                     <a class="print no-print btn btn-lg btn-primary" href="<?= $_SERVER['REQUEST_URI']."/".base64_encode("pdf");?>"><i class="fa fa-downlooad=o"></i> Download PDF</a>
