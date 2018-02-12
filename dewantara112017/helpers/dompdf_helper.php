e603078d40eba04e4cd0422aede4057912bc8f10
Syahroni Wahyu <roniwahyu@gmail.com>
2018-02-12 11:47:17 +0700

    fix download pdf alon-alon2

--- dewantara112017/helpers/dompdf_helper.php
+++ dewantara112017/helpers/dompdf_helper.php
@@ -5,18 +5,43 @@ function buildpdf($html, $filename='',$paper='A4',$orientation="landscape",$stre
     // use Dompdf\Dompdf();
     $options = new Dompdf\Options();
     // $fontMetrics = new Dompdf\FontMetrics();
-    // $options->setIsPhpEnabled(true);
+    $options->setIsPhpEnabled(true);
     $options->set(array(
         'pdfBackend'=>'PDFLib',
         'defaultMediaType'=>'screen',
         // 'defaultPaperSize'=>'A4',
         'defaultFont'=>'Helvetica',
         'enable_html5_parser'=>true,
-        'enable_font_subsetting'=>true
+        'enable_font_subsetting'=>true,
+        'isRemoteEnabled'=>TRUE
     ));
     $dompdf = new Dompdf\Dompdf($options);
+    if($paper=='A4'){
+        if($orientation=='portrait'){
+            $dompdf->set_paper(array(0,0,595,842));
+        }else{
+            // $dompdf->setPaper('A5','landscape');
+            $dompdf->set_paper(array(0,0,842,595));
+        }
+    }elseif($paper=='A5'){
+        if($orientation=='portrait'){
+            $dompdf->set_paper(array(0,0,420,595));
+        }elseif($orientation=='landscape'){
+            $dompdf->set_paper(array(0,0,595,420));
+        }
+    }elseif($paper=='A6'){
+        if($orientation=='portrait'){
+            $dompdf->set_paper(array(0,0,298,420));
+        }elseif($orientation=='landscape'){
+            $dompdf->set_paper(array(0,0,420,298));
+        }
+    }
     $dompdf->setPaper($paper, $orientation);
-    $dompdf->setBasePath(assets_url('css/bootstrap.min.css'));
+    // $dompdf->setBasePath(assets_url('css/bootstrap.min.css'));
+    // $dompdf->setBasePath(assets_url('images/logo.png'));
+    $image = assets_url('images/logo.png');
+    $canvas = $dompdf->get_canvas(); 
+    $canvas->image($image, 10, 10   , 50, 50);
     $dompdf->load_html($html);
     $dompdf->render();
     if ($stream==true) {
@@ -24,5 +49,8 @@ function buildpdf($html, $filename='',$paper='A4',$orientation="landscape",$stre
     } else {
         return $dompdf->output();
     }
+    unset($html);
+    unset($dompdf); 
 }
+
 ?>
