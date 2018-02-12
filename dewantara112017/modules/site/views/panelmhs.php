1eb79008464d5472ca5df2a9e9b0b68b7d4195bb
Syahroni Wahyu <roniwahyu@gmail.com>
2018-02-12 12:44:12 +0700

    update otentikasi setiap modul

---
+++ dewantara112017/modules/site/views/panelmhs.php
@@ -0,0 +1,21 @@
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+    <div class="widget style1 yellow-bg">
+        <a class="ahover" style="" href="<?php echo base_url('sika/mhs') ?>" data-load="<?php echo base_url('sika/mhs') ?>" data-table="<?php echo base_url('sika/mhs') ?>" data-remote-target="#ajax-remote">
+            <div class="row">
+                <div class="col-xs-12 text-center">
+                    <i class="fa fa-user fa-5x"></i>
+                </div>
+                <div class="col-xs-12 text-center">
+                    <h3 class="font-bold">&nbsp;</h3>
+                    <h3 class="font-bold">PANEL MAHASISWA</h3>
+                </div>
+            </div>
+        </a>
+        <div class="row" style="margin-top:20px">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <a class="btn btn-success" href=" <?php echo domain() ?>sika/matakuliah">Mata Kuliah</a>
+                <a class="btn btn-danger" href=" <?php echo domain() ?>sika/nilai">Nilai</a>
+            </div>
+        </div>
+    </div>
+</div>
--- dewantara112017/modules/site/views/site.php
+++ dewantara112017/modules/site/views/site.php
@@ -17,8 +17,7 @@ a.ahover:hover {
                     <h1>SIKA PGRI DEWANTARA</h1>
                     <h2>SISTEM INFORMASI AKADEMIK STIE PGRI DEWANTARA JOMBANG</h2>
                     <?php if($this->ion_auth->in_group(11)): ?>
-                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <div class="widget style1 navy-bg">
                             <a class="ahover" style="" href="<?php echo base_url('siku') ?>" data-load="<?php echo base_url('siku') ?>" data-table="<?php echo base_url('siku') ?>" data-remote-target="#ajax-remote">
                                 <div class="row">
@@ -37,13 +36,11 @@ a.ahover:hover {
                                         <a class="btn btn-success" href="<?php echo domain() ?>siku/jenis">Jenis Tarif</a>
                                         <a class="btn btn-info" href="<?php echo domain() ?>siku/kelompokmhs">Kelompok Mahasiswa</a>
                                         <a class="btn btn-warning" href="<?php echo domain() ?>siku/prodi">Program Studi</a>
-
                                     </div>
                                     <p></p>
                                 </div>
                                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                                     <div class="btn-group">
-                                       
                                         <a class="btn btn-warning" href=" <?php echo domain() ?>siku/tarif/">Tarif Dasar</a>
                                         <a class="btn btn-info" href=" <?php echo domain() ?>siku/paket_tarif/">Kelompok Tarif</a>
                                         <a class="btn btn-danger" href=" <?php echo domain() ?>siku/tagihan/">Tagihan</a>
@@ -53,32 +50,7 @@ a.ahover:hover {
                             </div>
                         </div>
                     </div>
-                <?php endif; ?>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        <div class="widget style1 blue-bg">
-                            <a class="ahover" style="" href="<?php echo base_url('pos') ?>" data-load="<?php echo base_url('pos') ?>" data-table="<?php echo base_url('pos') ?>" data-remote-target="#ajax-remote">
-                                <div class="row">
-                                    <div class="col-xs-12 text-center">
-                                        <i class="fa fa-graduation-cap fa-5x"></i>
-                                        <!-- <img src="<?php //echo assets_url() ?>/images/Inventory.png"> -->
-                                    </div>
-                                    <div class="col-xs-12 text-center">
-                                        <h3 class="font-bold">AKADEMIK</h3>
-                                    </div>
-                                </div>
-                            </a>
-                            <div class="row" style="margin-top:20px">
-                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                                    <div class="btn-group">
-                                        <a class="btn btn-info" href=" <?php echo domain() ?>sika/pt/">Profil PT</a>
-                                        <a class="btn btn-primary" href=" <?php echo domain() ?>sika/prodi">Profil Prodi</a>
-                                        <a class="btn btn-warning" href=" <?php echo domain() ?>sika/matakuliah">Mata Kuliah</a>
-                                        <a class="btn btn-danger" href=" <?php echo domain() ?>sika/nilai">Nilai</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <?php endif; ?>
                     <?php if($this->ion_auth->in_group(10)): ?>
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <div class="widget style1 yellow-bg">
@@ -107,6 +79,32 @@ a.ahover:hover {
                     <?php endif; ?>
                     <!-- <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3"> -->
                     <!-- </div> -->
+                    <?php if(!$this->ion_auth->in_group(array(9,8,7,5))): ?>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <div class="widget style1 blue-bg">
+                            <a class="ahover" style="" href="<?php echo base_url('pos') ?>" data-load="<?php echo base_url('pos') ?>" data-table="<?php echo base_url('pos') ?>" data-remote-target="#ajax-remote">
+                                <div class="row">
+                                    <div class="col-xs-12 text-center">
+                                        <i class="fa fa-graduation-cap fa-5x"></i>
+                                        <!-- <img src="<?php //echo assets_url() ?>/images/Inventory.png"> -->
+                                    </div>
+                                    <div class="col-xs-12 text-center">
+                                        <h3 class="font-bold">AKADEMIK</h3>
+                                    </div>
+                                </div>
+                            </a>
+                            <div class="row" style="margin-top:20px">
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                    <div class="btn-group">
+                                        <a class="btn btn-info" href=" <?php echo domain() ?>sika/pt/">Profil PT</a>
+                                        <a class="btn btn-primary" href=" <?php echo domain() ?>sika/prodi">Profil Prodi</a>
+                                        <a class="btn btn-warning" href=" <?php echo domain() ?>sika/matakuliah">Mata Kuliah</a>
+                                        <a class="btn btn-danger" href=" <?php echo domain() ?>sika/nilai">Nilai</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <div class="widget style1 red-bg">
                             <a class="ahover" style="" href="<?php echo base_url('farm') ?>" data-load="<?php echo base_url('farm') ?>" data-table="<?php echo base_url('farm') ?>" data-remote-target="#ajax-remote">
@@ -135,6 +133,18 @@ a.ahover:hover {
                             </div>
                         </div>
                     </div>
+                    <?php else: //jika cmhs/mhs/pendaftar/dosen?>
+                    <?php if($this->ion_auth->in_group(7)): //mahasiswa ?>
+                    <?php $this->load->view('panelmhs'); ?>
+                    
+                    <?php elseif($this->ion_auth->in_group(8)): //Calon Mahasiswa, atau pendaftar sudah ujian?>
+                    <?php $this->load->view('panelcmhs'); ?>
+                    <?php elseif($this->ion_auth->in_group(9)): //Pendaftar ?> Pendaftar belum calon mhs
+                    <?php $this->load->view('panelregistrar'); ?>
+                    <?php elseif($this->ion_auth->in_group(5)): //dosen?> dosen
+                    <?php $this->load->view('paneldosen'); ?>
+                    <?php endif; ?>
+                    <?php endif; ?>
                     <!-- <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3"> -->
                     <!-- </div> -->
                 </div>
