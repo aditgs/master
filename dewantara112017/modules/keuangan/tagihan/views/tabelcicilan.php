f2deb13420bfded01951cd14279f467c8dad872c
Syahroni Wahyu <roniwahyu@gmail.com>
2018-02-24 22:46:58 +0700

    tabel cicilan baru

---
+++ dewantara112017/modules/keuangan/tagihan/views/tabelcicilan.php
@@ -0,0 +1,17 @@
+<h3>Detail Cicilan</h3>
+<table class="table table-condensed table-hover">
+	<thead>
+		<tr>
+			<th></th>
+		</tr>
+	</thead>
+	<?php if(isset($data)&&$data!=null): print_r($data);?>
+	<tbody>
+		<?php foreach ($data as $key => $value):?>
+		<tr>
+			<td></td>
+		</tr>
+		<?php endforeach; ?>
+	</tbody>
+	<?php endif; ?>
+</table>
